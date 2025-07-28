bafc036ed8f093bf7747205d0740d82dc1a21391
Nivedit Jain <niveditjain@outlook.com>
2025-07-29 00:21:05 +0530

    Refactor Runtime class to enforce node type and add start method (#105)
    * cleaned up the SDK
    
    * Refactor Runtime class to enforce node type and add start method
    
    - Updated the connect method to accept a List of BaseNode and added validation for nodes.
    - Introduced a new async start method for future implementation.

--- python-sdk/exospherehost/runtime.py
+++ python-sdk/exospherehost/runtime.py
@@ -1,5 +1,6 @@
 from asyncio import Queue
 from typing import List, TypeVar
+from .node import BaseNode
 
 T = TypeVar('T')
 
@@ -12,12 +13,27 @@ class Runtime:
         self._batch_size = batch_size
         self._connected = False
         self._state_queue = Queue(maxsize=2*batch_size)
+        self._nodes: List[BaseNode] = []
 
-    async def connect(self, nodes: List[T]):
-        pass
+    async def connect(self, nodes: List[BaseNode]):
+        self._validate_nodes(nodes)
+        self._nodes = nodes
 
     async def _enqueue(self, batch_size: int):
         pass
 
-    def _validate_nodes(self):
+    def _validate_nodes(self, nodes: List[BaseNode]):
+
+        invalid_nodes = []
+
+        for node in nodes:
+            if not isinstance(node, BaseNode):
+                invalid_nodes.append(f"{node.__class__.__name__}")
+
+        if invalid_nodes:
+            raise ValueError(f"Following nodes do not inherit from exospherehost.node.BaseNode: {invalid_nodes}")
+
+        return nodes
+
+    async def start(self):
         pass
